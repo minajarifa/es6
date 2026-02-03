f129ad544e8da8184afef0c2476f595360b8a806
Your Name <minajmiha02@gmail.com>
2026-02-03 09:20:17 +0600

    default-paramiter.js added

--- let-const.js
+++ let-const.js
@@ -1,1 +1,16 @@
-const name = "arifa"
+const name = "Bangladesh";
+const countryName = "DB " + name;
+// console.log(countryName);
+let price = 120;
+price = 150;
+// console.log(price);
+const charge = 150;
+const dress = ['shirt','pant','jacket'];
+dress.push('maflar')
+// console.log(dress)
+const student = {
+    name :'arifa',
+    markes: 50,
+}
+student.markes= 60
+console.log(student)
