a36d4818a670cbfd4e51197ca359c4d7b1cbf7c1
Your Name <minajmiha02@gmail.com>
2026-02-03 12:23:36 +0600

    arrow-function.js

--- arraw-function.js
+++ arraw-function.js
@@ -1,5 +1,19 @@
-add(10,10)
-console.log(num1, num2)
-function add(num1,num2){
-    return num1+ num2
-}
+// add(10,10)
+
+// function add(num1,num2){
+//     console.log(num1, num2)
+//     return num1+ num2
+// }
+
+const addition = function (num1, num2) {
+  return (num1, num2);
+};
+const add2 = (num1, num2) => {
+  (num1, num2);
+};
+const result = add2(10 + 10);
+// console.log(result);
+
+const multiply = (a , b)=>a*b
+const mult = multiply(4,50)
+console.log(mult)
