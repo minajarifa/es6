b47440e5b5c5b81618ec05ff3bcc7e481c6c73ad
Your Name <minajmiha02@gmail.com>
2026-02-03 11:04:45 +0600

    backtic added

---
+++ template-string.js
@@ -0,0 +1,14 @@
+// const poem = "am pata jorara" +"marbo cabuk corbe ghora"
+// const poem = "am pata jorara \n" +"marbo cabuk corbe ghora \n"
+// const poem = `am pata jorara 
+// marbo cabuk corbe ghora
+// ore bubu ghure dara 
+// asche amr pagla ghora` 
+// console.log(poem)
+
+function sum(sum1,sum2){
+    const result = sum1 + sum2;
+    const output = `sum of ${sum1} and ${sum2} is equal to ${result}`
+    console.log(output)
+}
+sum(10,20)
