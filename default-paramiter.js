19d5936cd2b470464d53d4dd2b2c114e33defc6a
Your Name <minajmiha02@gmail.com>
2026-02-03 09:54:24 +0600

    template-string.js added

---
+++ default-paramiter.js
@@ -0,0 +1,22 @@
+// function sum(sum1, sum2) {
+//   const total = sum1 + sum2;
+//   console.log(total);
+// }
+// sum(10 , 20);
+
+
+// function sum(sum1=0,sum2=0){
+//     const total = sum1+sum2;
+//     // console.log(sum1,sum2,total);
+// }
+// sum(34)
+// function multiply(sum1=1,sum2=1){
+//     const result = sum1*sum2;
+//     console.log(result);
+// }
+// multiply(34)
+function fullname(first="" , last=""){
+    const name = first+" "+last;
+    console.log(name)
+}
+fullname("arifa")
