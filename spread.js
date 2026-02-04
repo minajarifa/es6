f2329e73618b9377d5b3489067202f0f33e5e3f2
Your Name <minajmiha02@gmail.com>
2026-02-04 20:25:22 +0600

    array

--- spread.js
+++ spread.js
@@ -1,9 +1,9 @@
 const number = [12, 13, 14, 15, 16, 17, 1118];
-console.log(number);
-console.log(...number);
+// console.log(number);
+// console.log(...number);
 // const max= Math.max(123,1456,356,23,45,78,90)
 const max = Math.max(...number);
-console.log(max);
+// console.log(max);
 
 // const first = [1,2,3,4,5,6,7,8,9];
 // const second = first;
@@ -11,12 +11,32 @@ console.log(max);
 // console.log(first)
 
 
-const first = [1,2,3,4,5,6,7,8,9];
-const second = [...first];
-second.push(10);
-console.log("first",first)
-console.log("second",second)
+// const first = [1,2,3,4,5,6,7,8,9];
+// const second = [...first];
+// const third = [...first,10,11,12,13,14,15,16];
+// second.push(10);
+// third.push(1000);
+// console.log("first",first)
+// console.log("second",second)
+// console.log("third",third)
 
 
+// const age = [11,12,13,14,115,16,17,18]
+// const price = [21,22,23,24,25,26,27,28]
+// const all = [...age,-10000000000,...price]
+// console.log(age)
+// console.log(price)
+// console.log(all)
 
-// console.log(second)
+
+// const person={name:"arifa",age:20}
+// const employ={designetion:"dev",...person}
+// console.log(employ)
+
+
+// const total=(a,b,c)=>a+b+c
+// const result = total(1,2,3);
+// const digit = [12,13,14]
+// total(...digit)
+// console.log(result)
+// console.log(digit)
