60d6ba9c26282c63ae641f36371f88fed2bc7e7a
Your Name <minajmiha02@gmail.com>
2026-02-04 19:32:18 +0600

    more-arrow.js

--- more-arrow.js
+++ more-arrow.js
@@ -1,6 +1,10 @@
 const getTex = (ammount, textrate) => (ammount * textrate) / 100;
 const add = (p, q) => p + q;
 const getSquare = (x) => x * x;
-// console.log(getSquare(8))
 const getHalf = (num) => num / 2;
+const firstElement = (nums) => nums[0];
+// console.log(getSquare(8))
 console.log(getHalf(15));
+console.log("ei je ami ", firstElement([12, 13, 14, 15, 16]));
+const logIt =()=>console.log(78);
+logIt(20)
