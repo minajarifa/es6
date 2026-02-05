6e1e398911319379987dfabf78b7374e045aa44a
Your Name <minajmiha02@gmail.com>
2026-02-05 12:38:55 +0600

    end

--- object-looping.js
+++ object-looping.js
@@ -1,1 +1,14 @@
-const numbers=[1,2,3,4,5,6,7,8,9,10,11,12]
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+for (const num of numbers) {
+//   console.log(num);
+}
+const employee = {
+  name: "Raja Rani",
+  designation: "QA",
+  salary: "200000",
+  experience: 1,
+};
+for (const key in employee) {
+  const value = employee[key];
+//   console.log(key, value);
+}
