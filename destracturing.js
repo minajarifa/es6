6ff166ed72b550cd483c4d96ddd41569e103b160
Your Name <minajmiha02@gmail.com>
2026-02-05 08:17:53 +0600

    freeze.js

--- destracturing.js
+++ destracturing.js
@@ -3,8 +3,10 @@ const { name, price, quentity } = {
   price: 3000,
   quentity: 7,
 };
+const aro = [12,13,14]
+const [first , second, third] = [12,13,14]
 const discount = (price * 20) / 100;
 const yourPay = price - discount;
 const vat = (price * 7) / 100;
 const totalAmount = yourPay + vat;
-console.log(totalAmount);
+console.log(first);
---
+++ entries.js
@@ -0,0 +1,11 @@
+const employee ={
+    name:"Raja Rani",
+    designation:"QA",
+    salary:"200000",
+    experience:1,
+   
+}
+const keys = Object.keys(employee)
+const value = Object.values(employee)
+const entries = Object.entries(employee)
+console.log(entries)
